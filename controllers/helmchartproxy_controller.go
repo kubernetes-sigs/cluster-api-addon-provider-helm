bd2b651bedd8f52b449abf3b08d3b746ff6fcb74
Jonathan Tong <jt572@cornell.edu>
2022-04-11 16:24:36 -0400

    Add Delete logic in controller

--- controllers/helmchartproxy_controller.go
+++ controllers/helmchartproxy_controller.go
@@ -26,10 +26,12 @@ import (
 	helmCli "helm.sh/helm/v3/pkg/cli"
 	helmVals "helm.sh/helm/v3/pkg/cli/values"
 	helmGetter "helm.sh/helm/v3/pkg/getter"
+	"helm.sh/helm/v3/pkg/release"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	ctrlLog "sigs.k8s.io/controller-runtime/pkg/log"
 
 	addonsv1beta1 "cluster-api-addon-helm/api/v1beta1"
@@ -40,6 +42,8 @@ type HelmChartProxyReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
 }
+
+const finalizer = "addons.cluster.x-k8s.io"
 
 //+kubebuilder:rbac:groups=addons.cluster.x-k8s.io,resources=helmchartproxies,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=addons.cluster.x-k8s.io,resources=helmchartproxies/status,verbs=get;update;patch
@@ -67,10 +71,40 @@ func (r *HelmChartProxyReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 		return ctrl.Result{}, err
 	}
 
-	log.Println("Got HelmChartProxy:", helmChartProxy.Name)
-	err := installChartWithHelm(ctx, helmChartProxy.Spec)
+	// examine DeletionTimestamp to determine if object is under deletion
+	if helmChartProxy.ObjectMeta.DeletionTimestamp.IsZero() {
+		// The object is not being deleted, so if it does not have our finalizer,
+		// then lets add the finalizer and update the object. This is equivalent
+		// registering our finalizer.
+		if !controllerutil.ContainsFinalizer(helmChartProxy, finalizer) {
+			controllerutil.AddFinalizer(helmChartProxy, finalizer)
+			if err := r.Update(ctx, helmChartProxy); err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+	} else {
+		// The object is being deleted
+		if controllerutil.ContainsFinalizer(helmChartProxy, finalizer) {
+			// our finalizer is present, so lets handle any external dependency
+			if err := r.reconcileDelete(ctx, helmChartProxy); err != nil {
+				// if fail to delete the external dependency here, return with error
+				// so that it can be retried
+				return ctrl.Result{}, err
+			}
+
+			// remove our finalizer from the list and update it.
+			controllerutil.RemoveFinalizer(helmChartProxy, finalizer)
+			if err := r.Update(ctx, helmChartProxy); err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+
+		// Stop reconciliation as the item is being deleted
+		return ctrl.Result{}, nil
+	}
+
+	err := r.reconcileNormal(ctx, helmChartProxy)
 	if err != nil {
-		log.Println("Error installing chart with Helm:", err)
 		return ctrl.Result{}, err
 	}
 
@@ -84,12 +118,69 @@ func (r *HelmChartProxyReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		Complete(r)
 }
 
-func installChartWithHelm(ctx context.Context, spec addonsv1beta1.HelmChartProxySpec) error {
-	log.Println("Prepared to install chart spec", spec)
+// reconcileNormal...
+func (r *HelmChartProxyReconciler) reconcileNormal(ctx context.Context, helmChartProxy *addonsv1beta1.HelmChartProxy) error {
+	if _, err := getHelmRelease(ctx, helmChartProxy.Spec); err != nil {
+		log.Println("Error getting chart:", err)
+	}
+
+	// TODO: Handle error when it's run on an existing release.
+	if _, err := installHelmRelease(ctx, helmChartProxy.Spec); err != nil {
+		log.Println("Error installing chart with Helm:", err)
+		return err
+	}
+
+	releases, err := listHelmReleases(ctx)
+	if err != nil {
+		log.Println("Error listing charts with Helm:", err)
+		return err
+	}
+
+	log.Println("Helm releases:")
+	limit := 200
+	for i, release := range releases {
+		log.Printf("%d. %s:\n", i, release.Name)
+		if len(release.Manifest) > limit {
+			log.Printf("%s\n...\n", release.Manifest[0:limit])
+		} else {
+			log.Println(release.Manifest)
+		}
+
+	}
+
+	return nil
+}
+
+// reconcileDelete...
+func (r *HelmChartProxyReconciler) reconcileDelete(ctx context.Context, helmChartProxy *addonsv1beta1.HelmChartProxy) error {
+	log.Println("Prepared to uninstall chart spec", helmChartProxy.Spec)
+
+	response, err := uninstallHelmRelease(ctx, helmChartProxy.Spec)
+	if err != nil {
+		log.Println("Error uninstalling chart with Helm:", err)
+	}
+
+	if response != nil && response.Info != "" {
+		log.Printf("Response is %s\n", response.Info)
+	}
+
+	return nil
+}
+
+func helmInit() (*helmCli.EnvSettings, *helmAction.Configuration, error) {
 	settings := helmCli.New()
 	actionConfig := new(helmAction.Configuration)
 	if err := actionConfig.Init(settings.RESTClientGetter(), "default", os.Getenv("HELM_DRIVER"), log.Printf); err != nil {
-		return err
+		return nil, nil, err
+	}
+
+	return settings, actionConfig, nil
+}
+
+func installHelmRelease(ctx context.Context, spec addonsv1beta1.HelmChartProxySpec) (*release.Release, error) {
+	settings, actionConfig, err := helmInit()
+	if err != nil {
+		return nil, err
 	}
 	installer := helmAction.NewInstall(actionConfig)
 	installer.RepoURL = spec.RepoURL
@@ -97,43 +188,74 @@ func installChartWithHelm(ctx context.Context, spec addonsv1beta1.HelmChartProxy
 	cp, err := installer.ChartPathOptions.LocateChart(spec.ChartReference, settings)
 	log.Println("Located chart at path", cp)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	p := helmGetter.All(settings)
 	valueOpts := &helmVals.Options{}
 	// valueOpts.Values = []string{fmt.Printf("infra.clusterName=%s", input.ClusterProxy.GetName())
 	vals, err := valueOpts.MergeValues(p)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	chartRequested, err := helmLoader.Load(cp)
-	chartRequested.Metadata.Name = spec.ReleaseName // TODO: remove this if not needed
-	// log.Printf("Requested chart is %+v\n", *chartRequested)
-	// log.Printf("Chart name is %s\n", chartRequested.Name())
-	// log.Printf("Values are is %+v\n", vals)
+	// chartRequested.Metadata.Name = spec.ReleaseName // TODO: remove this if not needed
 
 	if err != nil {
-		return err
+		return nil, err
 	}
 	log.Println("Installing with Helm...")
 	release, err := installer.RunWithContext(ctx, chartRequested, vals)
 	if err != nil {
-		log.Println("Error installing with Helm")
-		return err
+		return nil, err
 	}
 
-	log.Printf("Release %s is %+v\n", release.Name, release)
-	ls := helmAction.NewList(actionConfig)
-	releases, err := ls.Run()
+	log.Println("Released", release.Name)
+
+	return release, nil
+}
+
+func getHelmRelease(ctx context.Context, spec addonsv1beta1.HelmChartProxySpec) (*release.Release, error) {
+	_, actionConfig, err := helmInit()
 	if err != nil {
-		log.Println("Error listing releases with Helm")
-		return nil
+		return nil, err
 	}
-	for _, release := range releases {
-		log.Printf("Release %s is %+v\n", release.Name, release)
+	getter := helmAction.NewGet(actionConfig)
+	release, err := getter.Run(spec.ReleaseName)
+	if err != nil {
+		return nil, err
 	}
 
-	// helmAction.NewUninstall()
+	return release, nil
+}
 
-	return nil
+func listHelmReleases(ctx context.Context) ([]*release.Release, error) {
+	_, actionConfig, err := helmInit()
+	if err != nil {
+		return nil, err
+	}
+	lister := helmAction.NewList(actionConfig)
+	releases, err := lister.Run()
+	if err != nil {
+		log.Println("Error listing releases with Helm:", err)
+		return nil, err
+	}
+
+	return releases, nil
+}
+
+func uninstallHelmRelease(ctx context.Context, spec addonsv1beta1.HelmChartProxySpec) (*release.UninstallReleaseResponse, error) {
+	_, actionConfig, err := helmInit()
+	if err != nil {
+		return nil, err
+	}
+
+	uninstaller := helmAction.NewUninstall(actionConfig)
+	response, err := uninstaller.Run(spec.ReleaseName)
+	if err != nil {
+		log.Println("Failed to uninstall chart", spec.ReleaseName)
+		log.Println(err.Error())
+		return nil, err
+	}
+
+	return response, nil
 }
--- go.mod
+++ go.mod
@@ -45,7 +45,6 @@ require (
 	github.com/evanphx/json-patch v4.12.0+incompatible // indirect
 	github.com/exponent-io/jsonpath v0.0.0-20151013193312-d6023ce2651d // indirect
 	github.com/fatih/color v1.13.0 // indirect
-	github.com/form3tech-oss/jwt-go v3.2.3+incompatible // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/go-errors/errors v1.0.1 // indirect
 	github.com/go-logr/logr v1.2.0 // indirect
