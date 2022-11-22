74633c4dee75397f08d91ea3a6f0425a8a876d81
Jonathan Tong <jt572@cornell.edu>
2022-11-22 18:40:28 -0500

    Update boilerplate header

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2022.
+Copyright 2022 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the addons v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=addons.cluster.x-k8s.io
+// +kubebuilder:object:generate=true
+// +groupName=addons.cluster.x-k8s.io
 package v1alpha1
 
 import (
