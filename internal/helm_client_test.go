ba6fb65625913867375e2158f8ba7b1ba1def1a1
Matt Boersma <Matt.Boersma@microsoft.com>
2026-03-23 10:45:50 -0600

    Add unit tests for generateHelmInstallConfig and generateHelmUpgradeConfig

--- internal/helm_client_test.go
+++ internal/helm_client_test.go
@@ -20,9 +20,14 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/types"
+	helmAction "helm.sh/helm/v3/pkg/action"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	addonsv1alpha1 "sigs.k8s.io/cluster-api-addon-provider-helm/api/v1alpha1"
 )
 
 func TestNewDefaultRegistryClient(t *testing.T) {
@@ -99,6 +104,178 @@ func TestNewDefaultRegistryClient(t *testing.T) {
 			)
 			g.Expect(err).To(tc.assertErr)
 			g.Expect(client).To(tc.assertClient)
+		})
+	}
+}
+
+func TestGenerateHelmInstallConfig(t *testing.T) {
+	t.Parallel()
+
+	timeout := &metav1.Duration{Duration: 5 * time.Minute}
+
+	testCases := []struct {
+		name        string
+		helmOptions *addonsv1alpha1.HelmOptions
+		assert      func(*GomegaWithT, *helmAction.Install)
+	}{
+		{
+			name:        "nil helmOptions returns defaults",
+			helmOptions: nil,
+			assert: func(g *GomegaWithT, install *helmAction.Install) {
+				g.Expect(install.CreateNamespace).To(BeTrue())
+				g.Expect(install.TakeOwnership).To(BeFalse())
+				g.Expect(install.DisableHooks).To(BeFalse())
+				g.Expect(install.Wait).To(BeFalse())
+			},
+		},
+		{
+			name:        "empty helmOptions",
+			helmOptions: &addonsv1alpha1.HelmOptions{},
+			assert: func(g *GomegaWithT, install *helmAction.Install) {
+				g.Expect(install.TakeOwnership).To(BeFalse())
+				g.Expect(install.CreateNamespace).To(BeFalse())
+				g.Expect(install.DisableHooks).To(BeFalse())
+			},
+		},
+		{
+			name: "TakeOwnership set to true",
+			helmOptions: &addonsv1alpha1.HelmOptions{
+				TakeOwnership: true,
+			},
+			assert: func(g *GomegaWithT, install *helmAction.Install) {
+				g.Expect(install.TakeOwnership).To(BeTrue())
+			},
+		},
+		{
+			name: "all options set",
+			helmOptions: &addonsv1alpha1.HelmOptions{
+				DisableHooks:             true,
+				Wait:                     true,
+				WaitForJobs:              true,
+				Timeout:                  timeout,
+				SkipCRDs:                 true,
+				SubNotes:                 true,
+				DisableOpenAPIValidation: true,
+				Atomic:                   true,
+				TakeOwnership:            true,
+				Install: addonsv1alpha1.HelmInstallOptions{
+					CreateNamespace: true,
+					IncludeCRDs:     true,
+				},
+			},
+			assert: func(g *GomegaWithT, install *helmAction.Install) {
+				g.Expect(install.DisableHooks).To(BeTrue())
+				g.Expect(install.Wait).To(BeTrue())
+				g.Expect(install.WaitForJobs).To(BeTrue())
+				g.Expect(install.Timeout).To(Equal(5 * time.Minute))
+				g.Expect(install.SkipCRDs).To(BeTrue())
+				g.Expect(install.SubNotes).To(BeTrue())
+				g.Expect(install.DisableOpenAPIValidation).To(BeTrue())
+				g.Expect(install.Atomic).To(BeTrue())
+				g.Expect(install.TakeOwnership).To(BeTrue())
+				g.Expect(install.CreateNamespace).To(BeTrue())
+				g.Expect(install.IncludeCRDs).To(BeTrue())
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			g := NewWithT(t)
+			actionConfig := &helmAction.Configuration{}
+			install := generateHelmInstallConfig(actionConfig, tc.helmOptions)
+			g.Expect(install).ToNot(BeNil())
+			tc.assert(g, install)
+		})
+	}
+}
+
+func TestGenerateHelmUpgradeConfig(t *testing.T) {
+	t.Parallel()
+
+	timeout := &metav1.Duration{Duration: 5 * time.Minute}
+
+	testCases := []struct {
+		name        string
+		helmOptions *addonsv1alpha1.HelmOptions
+		assert      func(*GomegaWithT, *helmAction.Upgrade)
+	}{
+		{
+			name:        "nil helmOptions returns defaults",
+			helmOptions: nil,
+			assert: func(g *GomegaWithT, upgrade *helmAction.Upgrade) {
+				g.Expect(upgrade.TakeOwnership).To(BeFalse())
+				g.Expect(upgrade.DisableHooks).To(BeFalse())
+				g.Expect(upgrade.Wait).To(BeFalse())
+			},
+		},
+		{
+			name:        "empty helmOptions",
+			helmOptions: &addonsv1alpha1.HelmOptions{},
+			assert: func(g *GomegaWithT, upgrade *helmAction.Upgrade) {
+				g.Expect(upgrade.TakeOwnership).To(BeFalse())
+				g.Expect(upgrade.DisableHooks).To(BeFalse())
+			},
+		},
+		{
+			name: "TakeOwnership set to true",
+			helmOptions: &addonsv1alpha1.HelmOptions{
+				TakeOwnership: true,
+			},
+			assert: func(g *GomegaWithT, upgrade *helmAction.Upgrade) {
+				g.Expect(upgrade.TakeOwnership).To(BeTrue())
+			},
+		},
+		{
+			name: "all options set",
+			helmOptions: &addonsv1alpha1.HelmOptions{
+				DisableHooks:             true,
+				Wait:                     true,
+				WaitForJobs:              true,
+				Timeout:                  timeout,
+				SkipCRDs:                 true,
+				SubNotes:                 true,
+				DisableOpenAPIValidation: true,
+				Atomic:                   true,
+				TakeOwnership:            true,
+				Upgrade: addonsv1alpha1.HelmUpgradeOptions{
+					Force:                true,
+					ResetValues:          true,
+					ReuseValues:          true,
+					ResetThenReuseValues: true,
+					MaxHistory:           10,
+					CleanupOnFail:        true,
+				},
+			},
+			assert: func(g *GomegaWithT, upgrade *helmAction.Upgrade) {
+				g.Expect(upgrade.DisableHooks).To(BeTrue())
+				g.Expect(upgrade.Wait).To(BeTrue())
+				g.Expect(upgrade.WaitForJobs).To(BeTrue())
+				g.Expect(upgrade.Timeout).To(Equal(5 * time.Minute))
+				g.Expect(upgrade.SkipCRDs).To(BeTrue())
+				g.Expect(upgrade.SubNotes).To(BeTrue())
+				g.Expect(upgrade.DisableOpenAPIValidation).To(BeTrue())
+				g.Expect(upgrade.Atomic).To(BeTrue())
+				g.Expect(upgrade.TakeOwnership).To(BeTrue())
+				g.Expect(upgrade.Force).To(BeTrue())
+				g.Expect(upgrade.ResetValues).To(BeTrue())
+				g.Expect(upgrade.ReuseValues).To(BeTrue())
+				g.Expect(upgrade.ResetThenReuseValues).To(BeTrue())
+				g.Expect(upgrade.MaxHistory).To(Equal(10))
+				g.Expect(upgrade.CleanupOnFail).To(BeTrue())
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			g := NewWithT(t)
+			actionConfig := &helmAction.Configuration{}
+			upgrade := generateHelmUpgradeConfig(actionConfig, tc.helmOptions)
+			g.Expect(upgrade).ToNot(BeNil())
+			tc.assert(g, upgrade)
 		})
 	}
 }
